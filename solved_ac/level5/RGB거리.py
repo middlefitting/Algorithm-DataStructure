8ce450d4b62b5ef2359be6bfdec6ff854fcd1f91
middlefitting <wjdtmdcjf2@gmail.com>
2022-10-02 01:48:31 +0900

    UPDATE : RGB거리2.py

--- solved_ac/level5/RGB거리.py
+++ solved_ac/level5/RGB거리.py
@@ -1,19 +1,20 @@
-INF = 2147000000
-n = int(input())
-rgb = []
-ans = INF
-for _ in range(n):
-    rgb.append(list(map(int, input().split())))
+import sys
+
+N = int(sys.stdin.readline())
+result = 1000 * 1000
+rgb_distance = []
+for i in range(N):
+    rgb_distance.append(list(map(int, sys.stdin.readline().split())))
 
 for i in range(3):
-    dp = [[INF, INF, INF] for _ in range(n)]
-    dp[0][i] = rgb[0][i]
-    for j in range(1, n):
-        dp[j][0] = rgb[j][0] + min(dp[j-1][1], dp[j-1][2])
-        dp[j][1] = rgb[j][1] + min(dp[j-1][0], dp[j-1][2])
-        dp[j][2] = rgb[j][2] + min(dp[j-1][0], dp[j-1][1])
-
+    dp = [[1000 * 1000] * 3 for _ in range(N)]
+    dp[0][i] = rgb_distance[0][i]
+    for j in range(1, N):
+        dp[j][0] = min(dp[j - 1][1], dp[j - 1][2]) + rgb_distance[j][0]
+        dp[j][1] = min(dp[j - 1][2], dp[j - 1][0]) + rgb_distance[j][1]
+        dp[j][2] = min(dp[j - 1][0], dp[j - 1][1]) + rgb_distance[j][2]
     for j in range(3):
         if i != j:
-            ans = min(ans, dp[-1][j])
-print(ans)
+            result = min(result, dp[N - 1][j])
+
+print(result)
