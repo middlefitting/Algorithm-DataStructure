7389e5de45ad891bad3fd4f3af021d74b886194d
middlefitting <wjdtmdcjf2@gmail.com>
2023-01-31 17:50:21 +0900

    FEAT : - 두배열의합.py

--- 코딩테스트준비-연습/브루트포스-기타/두배열의합.py
+++ 코딩테스트준비-연습/브루트포스-기타/두배열의합.py
@@ -1,35 +1,44 @@
 T = int(input())
+
+
+def make_arr(arr, N, X):
+    for i in range(N):
+        for j in range(i, N):
+            if i == j:
+                arr[i][j] = X[i]
+            else:
+                arr[i][j] = X[j] + arr[i][j - 1]
+
+
+def make_dict(dict, arr):
+    for i in range(N):
+        for j in range(i, N):
+            try:
+                dict[arr[i][j]] += 1
+            except:
+                dict[arr[i][j]] = 1
+
+
+def find_answer(dict, arr):
+    answer = 0
+    for i in range(M):
+        for j in range(i, M):
+            temp = T - arr[i][j]
+            try:
+                answer += dict[temp]
+            except:
+                continue
+    return answer
+
+
 N = int(input())
 A = list(map(int, input().split()))
 M = int(input())
 B = list(map(int, input().split()))
-answer = 0
 arrA = [[0] * N for _ in range(N)]
 arrB = [[0] * M for _ in range(M)]
 dictA = dict()
-for i in range(N):
-    for j in range(i, N):
-        if i == j:
-            arrA[i][j] = A[i]
-        else:
-            arrA[i][j] = A[j] + arrA[i][j - 1]
-for i in range(M):
-    for j in range(i, M):
-        if i == j:
-            arrB[i][j] = B[i]
-        else:
-            arrB[i][j] = B[j] + arrB[i][j - 1]
-for i in range(N):
-    for j in range(i, N):
-        try:
-            dictA[arrA[i][j]] += 1
-        except:
-            dictA[arrA[i][j]] = 1
-for i in range(M):
-    for j in range(i, M):
-        temp = T - arrB[i][j]
-        try:
-            answer += dictA[temp]
-        except:
-            continue
-print(answer)
+make_arr(arrA, N, A)
+make_arr(arrB, M, B)
+make_dict(dictA, arrA)
+print(find_answer(dictA, arrB))
