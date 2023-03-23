deeacac23423c1d85ecd4616f63ef2fb43733e45
middlefitting <wjdtmdcjf2@gmail.com>
2023-03-23 12:38:51 +0900

    FEAT: 절대값힙.py

---
+++ Solved/단계별로풀어보기/우선순위큐/최대힙.py
@@ -0,0 +1,14 @@
+import heapq
+import sys
+
+N = int(input())
+heap = []
+for i in range(N):
+    temp = int(sys.stdin.readline())
+    if temp == 0:
+        if heap:
+            print(heapq.heappop(heap) * -1)
+        else:
+            print(0)
+    else:
+        heapq.heappush(heap, temp * -1)
