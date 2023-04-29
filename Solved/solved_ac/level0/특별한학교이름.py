4cbe03cc59f436ad6b8f160af4c225ab8ceff21e
middlefitting <wjdtmdcjf2@gmail.com>
2023-04-30 01:00:20 +0900

    FEAT: 특별한학교이름.py

---
+++ Solved/solved_ac/level0/특별한학교이름.py
@@ -0,0 +1,6 @@
+d = dict()
+d["NLCS"] = "North London Collegiate School"
+d["BHA"] = "Branksome Hall Asia"
+d["KIS"] = "Korea International School"
+d["SJA"] = "St. Johnsbury Academy"
+print(d[str(input())])
