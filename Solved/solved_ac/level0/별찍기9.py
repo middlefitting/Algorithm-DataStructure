9189634b7f42a99dc0bafdaeebd5c8673dc5c758
middlefitting <wjdtmdcjf2@gmail.com>
2023-07-07 22:34:32 +0900

    feat: 별찍기9.py

---
+++ Solved/solved_ac/level0/별찍기9.py
@@ -0,0 +1,7 @@
+N = int(input())
+for i in range(N):
+    print(" " * i, end="")
+    print("*" * ((N - i) * 2 - 1))
+for i in range(1, N):
+    print(" " * (N - i - 1), end="")
+    print("*" * ((i + 1) * 2 - 1))
