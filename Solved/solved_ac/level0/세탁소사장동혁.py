db28907a8a34ecb043e7b6203402252d6111c1d0
middlefitting <wjdtmdcjf2@gmail.com>
2023-08-06 11:33:32 +0900

    feat(solved): 세탁소사장동혁.py

---
+++ Solved/solved_ac/level0/세탁소사장동혁.py
@@ -0,0 +1,9 @@
+for _ in range(int(input())):
+    n = int(input())
+    print(n // 25, end=" ")
+    n %= 25
+    print(n // 10, end=" ")
+    n %= 10
+    print(n // 5, end=" ")
+    n %= 5
+    print(n)
