444cc9006b260ee084f257ad31ea09b25e074cf3
middlefitting <wjdtmdcjf2@gmail.com>
2023-08-10 15:55:21 +0900

    feat(solved): 아이들은사탕을좋아해.py

---
+++ Solved/solved_ac/level0/아이들은사탕을좋아해.py
@@ -0,0 +1,6 @@
+for _ in range(int(input())):
+    N, K = map(int, input().split())
+    arr = list(map(int, input().split()))
+    answer = 0
+    for i in range(N):
+        answer += (arr[i] // K)
