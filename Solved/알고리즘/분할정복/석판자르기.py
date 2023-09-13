16278e83979bdeb28b28db1dc2b654323bd2dff6
middlefitting <wjdtmdcjf2@gmail.com>
2023-09-13 17:52:36 +0900

    feat(solved): 석판자르기.py

---
+++ Solved/알고리즘/분할정복/석판자르기.py
@@ -0,0 +1,13 @@
+def cut(box, x1, x2, y1, y2, pos):
+    if pos == 1:
+        print()
+    elif pos == 2:
+        print()
+    else:
+
+
+
+N = int(input())
+arr = []
+for _ in range(N):
+    arr.append(list(map(int, input().split())))
