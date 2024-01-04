83b901ab28bcae578174af3837e693398cbf6f70
middlefitting <wjdtmdcjf2@gmail.com>
2024-01-04 19:29:05 +0900

    feat(solved.ac): 10102.py

---
+++ Solved/알고리즘/구현/10102.py
@@ -0,0 +1,14 @@
+s = str(input())
+A = 0
+B = 0
+for i in s:
+    if i == "A":
+        A += 1
+    else:
+        B += 1
+if A > B:
+    print("A")
+elif B > A:
+    print("B")
+else:
+    print("Tie")
