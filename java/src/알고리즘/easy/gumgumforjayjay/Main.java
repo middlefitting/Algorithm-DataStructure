884c79d278be58f886b5b4c26be4c3e5882185dd
middlefitting <wjdtmdcjf2@gmail.com>
2024-03-30 14:35:03 +0900

    feat(solved.ac): gumgumforjayjay

---
+++ java/src/알고리즘/easy/gumgumforjayjay/Main.java
@@ -0,0 +1,20 @@
+package 알고리즘.easy.gumgumforjayjay;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) {
+		try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+			int cnt = 0;
+			while (br.readLine() != null) {
+				cnt++;
+			}
+			System.out.println(cnt);
+		} catch (IOException e) {
+			e.printStackTrace();
+			throw new RuntimeException();
+		}
+	}
+}
