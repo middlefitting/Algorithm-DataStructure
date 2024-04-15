208cd47272080da98a2d87592d78af6979752386
middlefitting <wjdtmdcjf2@gmail.com>
2024-04-16 08:44:31 +0900

    feat(solved.ac): 1로 만들기

---
+++ java/src/알고리즘/dp/일로만들기/Main.java
@@ -0,0 +1,45 @@
+package 알고리즘.dp.일로만들기;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+	private static int n;
+
+	public static void main(String[] args) {
+		parse();
+		logic();
+	}
+
+	private static void logic() {
+		int[] dp = new int[n + 1];
+		Arrays.fill(dp, 0);
+		if (n >= 2) {
+			dp[2] = 1;
+		}
+		if (n >= 3) {
+			dp[3] = 1;
+		}
+
+		for (int i = 4; i <= n; i++) {
+			dp[i] = dp[i - 1] + 1;
+			if (i % 2 == 0) {
+				dp[i] = Math.min(dp[i], dp[i / 2] + 1);
+			}
+			if (i % 3 == 0) {
+				dp[i] = Math.min(dp[i], dp[i / 3] + 1);
+			}
+		}
+		System.out.println(dp[n]);
+	}
+
+	private static void parse() {
+		try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+			n = Integer.parseInt(br.readLine());
+		} catch (IOException e) {
+			throw new RuntimeException();
+		}
+	}
+}
