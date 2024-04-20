9a161502438741ca84df38ba4935d98190ff4739
middlefitting <wjdtmdcjf2@gmail.com>
2024-04-20 12:32:21 +0900

    feat(solced.ac): 정렬

--- java/src/랜덤디팬스/실버/정렬/Main.java
+++ java/src/랜덤디팬스/실버/정렬/Main.java
@@ -10,64 +10,33 @@ public class Main {
 		try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
 			int n = Integer.parseInt(br.readLine());
 			long[] arr = Arrays.stream(br.readLine().split(" ")).mapToLong(Long::parseLong).toArray();
-			long[] sort = Arrays.stream(arr).toArray();
-			Arrays.sort(arr);
-			int flag = 1;
-			int dir = -1;
-			for (int i = 0; i < n - 1; i++) {
-				if (arr[i] != sort[i]) {
-					if (flag == 1) {
-						flag = 0;
-						if (i == (n - 2)) {
-							flag = 2;
-						}
-					} else if(flag == 0) {
-						if (dir == -1) {
-							if (arr[i] == sort[i + 1]) {
-								dir = 1;
-							} else if (sort[i] == arr[i + 1]) {
-								dir = 2;
-							} else {
-								flag = -1;
-								break;
-							}
-							if (i == (n - 1)) {
-								flag = 2;
-							}
-						} else {
-							if (dir == 1 && arr[i] != sort[i + 1]) {
-								flag = -1;
-								break;
-							} else if (dir == 2 && sort[i] != arr[i + 1]) {
-								flag = -1;
-								break;
-							}
-						}
-					}
+			int target = 0;
+			long max = arr[0];
+			int idx = 0;
+			for (int i = 1; i < n; i++) {
+				if (arr[i - 1] > arr[i]) {
+					target++;
+					idx = i;
+				} else {
+					max = arr[i];
 				}
 			}
-
-			if (flag == 1) {
-				System.out.println(n);
-			} else if (flag == 2) {
-				System.out.println(2);
-			} else if (flag == 0) {
-				System.out.println(1);
-			} else {
+			if (target >= 2) {
 				System.out.println(0);
+			} else if (target == 0) {
+				System.out.println(n);
+			} else if (target == 1) {
+				int result = 0;
+				if (idx == (n - 1) || arr[idx + 1] >= arr[idx - 1]) {
+					result++;
+				}
+				if ((idx - 1) == 0 || arr[idx - 2] <= arr[idx]) {
+					result++;
+				}
+				System.out.println(result);
 			}
-
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
 }
-
-// 1 3 5 2
-// 1 2 3 5
-
-// 1 5 2 2
-// 1 2 2 5
-
-// 1 2 3 2
-// 1 2 2 3
