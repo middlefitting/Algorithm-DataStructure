86bc8150bda27f4b507980d1fd2689e74eacd1ec
middlefitting <wjdtmdcjf2@gmail.com>
2024-06-08 17:02:13 +0900

    feat(solved.ac): 유학금지

---
+++ java/src/유형/문자열/유학금지/Main.java
@@ -0,0 +1,30 @@
+package 유형.문자열.유학금지;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) {
+		try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+			char[] arr = {'C', 'A', 'M', 'B', 'R', 'I', 'D', 'G', 'E'};
+			String result = "";
+			String s = br.readLine();
+			for (int i = 0; i < s.length(); i++) {
+				boolean flag = true;
+				char temp = s.charAt(i);
+				for (char c : arr) {
+					if (temp == c) {
+						flag = false;
+					}
+				}
+				if (flag) {
+					result += temp;
+				}
+			}
+			System.out.println(result);
+		} catch (IOException e) {
+			throw new RuntimeException();
+		}
+	}
+}
