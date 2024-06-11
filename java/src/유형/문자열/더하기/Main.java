f9e9a3ac430725a67ff8df6d46ed9b32e21041a4
middlefitting <wjdtmdcjf2@gmail.com>
2024-06-11 13:23:43 +0900

    feat(solved.ac): 더하기

---
+++ java/src/유형/문자열/더하기/Main.java
@@ -0,0 +1,17 @@
+package 유형.문자열.더하기;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+	public static void main(String[] args) {
+		try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+			int[] arr = Arrays.stream(br.readLine().split(",")).mapToInt(Integer::parseInt).toArray();
+			System.out.println(Arrays.stream(arr).sum());
+		} catch (IOException e) {
+			throw new RuntimeException();
+		}
+	}
+}
