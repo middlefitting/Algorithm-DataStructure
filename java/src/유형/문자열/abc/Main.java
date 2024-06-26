a9365a7261674c35fef339589a5432dc1dbf3e4b
middlefitting <wjdtmdcjf2@gmail.com>
2024-06-26 23:33:35 +0900

    feat(solved.ac): abc

---
+++ java/src/유형/문자열/abc/Main.java
@@ -0,0 +1,20 @@
+package 유형.문자열.abc;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) {
+		try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+			String a = br.readLine();
+			String b = br.readLine();
+			String c = br.readLine();
+
+			System.out.println(Integer.parseInt(a) + Integer.parseInt(b) - Integer.parseInt(c));
+			System.out.println(Integer.parseInt(a + b) - Integer.parseInt(c));
+		} catch (IOException e) {
+			throw new RuntimeException();
+		}
+	}
+}
