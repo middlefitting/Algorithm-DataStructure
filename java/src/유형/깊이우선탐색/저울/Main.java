c21bc548fbf43675a4e6dd8f649bb41f69c08048
middlefitting <wjdtmdcjf2@gmail.com>
2024-07-20 11:20:34 +0900

    feat(solved.ac): 저울

--- java/src/유형/깊이우선탐색/저울/Main.java
+++ java/src/유형/깊이우선탐색/저울/Main.java
@@ -3,28 +3,28 @@ package 유형.깊이우선탐색.저울;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.StringTokenizer;
-import java.util.Vector;
 
 public class Main {
+	static int[][] dist;
 
-	static boolean[] v;
-	static int[] cnt;
-	static Vector<Integer>[] vec;
+	static int MAX = 10000000;
 
 	public static void main(String[] args) {
 		try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
 			int n = Integer.parseInt(br.readLine());
 			int m = Integer.parseInt(br.readLine());
 
-			v = new boolean[n + 1];
-			cnt = new int[n + 1];
-			Arrays.fill(cnt, 1);
-			vec = new Vector[n + 1];
-			for (int i = 0; i <= n; i++) {
-				vec[i] = new Vector<>();
+			dist = new int[n + 1][n + 1];
+
+			for (int i = 1; i <= n; i++) {
+				for (int j = 1; j <= n; j++) {
+					if (i == j) {
+						dist[i][j] = 0;
+					} else {
+						dist[i][j] = MAX;
+					}
+				}
 			}
 
 			StringTokenizer st;
@@ -32,34 +32,34 @@ public class Main {
 				st = new StringTokenizer(br.readLine());
 				int a = Integer.parseInt(st.nextToken());
 				int b = Integer.parseInt(st.nextToken());
-				vec[a].add(b);
+				dist[a][b] = 1;
 			}
 
-			for (int i = 1; i <= n; i++) {
-				if (!v[i]) {
-					ArrayList<Integer> arr = new ArrayList<>();
-					arr.add(i);
-					dfs(arr, i);
+			for (int k = 1; k <= n; k++) {
+				for (int i = 1; i <= n; i++) {
+					for (int j = 1; j <= n; j++) {
+						if (dist[i][j] > dist[i][k] + dist[k][j]) {
+							dist[i][j] = dist[i][k] + dist[k][j];
+						}
+					}
 				}
 			}
 
-
+			for (int i = 1; i <= n; i++) {
+				int cnt = 0;
+				for (int j = 1; j <= n; j++) {
+					if (i != j && dist[i][j] == MAX && dist[j][i] == MAX) {
+						cnt++;
+					}
+				}
+				System.out.println(cnt);
+			}
 
 		} catch (IOException e) {
 			throw new RuntimeException();
 		}
 	}
 
-	private static void dfs(ArrayList<Integer> arr, int i) {
-		v[i] = true;
-		for (int child : vec[i]) {
-			if (!v[child]) {
-				dfs(arr, child);
-			}
-			cnt[i] += cnt[child];
-			cnt[child] += 1;
-		}
-	}
 }
 
 
