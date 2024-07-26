864f1cf5b04618f704db95c005c31ddebf156048
middlefitting <wjdtmdcjf2@gmail.com>
2024-07-26 21:20:18 +0900

    feat(solved.ac): 관공

---
+++ java/src/유형/문자열/관공/Main.java
@@ -0,0 +1,21 @@
+package 유형.문자열.관공;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) {
+		try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+			int n = Integer.parseInt(br.readLine());
+			for (int i = 0; i < n; i++) {
+				String s = br.readLine();
+				if (s.contains("S")) {
+					System.out.println(s);
+				}
+			}
+		} catch (IOException e) {
+			throw new RuntimeException();
+		}
+	}
+}
