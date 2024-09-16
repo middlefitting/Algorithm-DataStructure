ceb1964dca2a7250320a165d532cfb2beea48717
middlefitting <wjdtmdcjf2@gmail.com>
2024-09-16 22:36:38 +0900

    feat(solved.ac): who is the middle

---
+++ java/src/유형/구현/whoisthemiddle/Main.java
@@ -0,0 +1,21 @@
+package 유형.구현.whoisthemiddle;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+	public static void main(String[] args) {
+		try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+			int[] arr = new int[3];
+			for (int i = 0; i < 3; i++) {
+				arr[i] = Integer.parseInt(br.readLine());
+			}
+			Arrays.sort(arr);
+			System.out.println(arr[1]);
+		} catch (IOException e) {
+			throw new RuntimeException();
+		}
+	}
+}
