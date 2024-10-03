a643e977521619bb69f9b135495da991ccc5dab4
middlefitting <wjdtmdcjf2@gmail.com>
2024-10-03 15:28:42 +0900

    feat(solved.ac): 삼각형

---
+++ java/src/유형/구현/삼각형/Main.java
@@ -0,0 +1,19 @@
+package 유형.구현.삼각형;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) {
+		try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			double n = Double.parseDouble(st.nextToken());
+			double m = Double.parseDouble(st.nextToken());
+			System.out.println(n * m / 2);
+		} catch (IOException e) {
+			throw new RuntimeException();
+		}
+	}
+}
