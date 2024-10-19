9bf7eca79d74c0d97e3b97e004947e7836cac38c
middlefitting <wjdtmdcjf2@gmail.com>
2024-10-19 22:36:49 +0900

    feat(solved.ac): 카드

---
+++ java/src/유형/해시/카드/Main.java
@@ -0,0 +1,33 @@
+package 유형.해시.카드;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+
+public class Main {
+	public static void main(String[] args) {
+		try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+			int n = Integer.parseInt(br.readLine());
+
+			HashMap<Long, Integer> map = new HashMap<>();
+			long result = 0;
+			for (int i = 0; i < n; i++) {
+				long num = Long.parseLong(br.readLine());
+				result = num;
+				map.put(num, map.getOrDefault(num, 0) + 1);
+			}
+
+			for (long i : map.keySet()) {
+				if (map.get(result) < map.get(i)) {
+					result = i;
+				}
+			}
+
+			System.out.println(result);
+
+		} catch (IOException e) {
+			throw new RuntimeException();
+		}
+	}
+}
