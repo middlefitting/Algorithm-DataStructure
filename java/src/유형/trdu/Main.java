f6679cfb8a8eff69330be3207969b135368b072a
middlefitting <wjdtmdcjf2@gmail.com>
2024-11-24 16:53:46 +0900

    feat(solved.ac): trdu

---
+++ java/src/유형/trdu/Main.java
@@ -0,0 +1,25 @@
+package 유형.trdu;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) {
+		try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+
+			if (a == b) {
+				System.out.println(a);
+			} else {
+				System.out.println(Math.max(a, b));
+			}
+		} catch (IOException e) {
+			throw new RuntimeException();
+		}
+	}
+}
