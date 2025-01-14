52ce6f11087853dcfb7fe89f39d89a9abe5c9709
middlefitting <wjdtmdcjf2@gmail.com>
2025-01-14 23:14:53 +0900

    feat(solved.ac): 골뱅이찍기2

---
+++ java/src/유형/골뱅이찍기2/Main.java
@@ -0,0 +1,46 @@
+package 유형.골뱅이찍기2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) {
+		try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+
+			int n = Integer.parseInt(br.readLine());
+
+			StringBuilder sb = new StringBuilder();
+			for (int i = 0; i < n; i++) {
+				for (int j = 0; j < n * 5; j++) {
+					sb.append("@");
+				}
+				sb.append("\n");
+			}
+
+			for (int i = 0; i < n * 3; i++) {
+				for (int j = 0; j < n; j++) {
+					sb.append("@");
+				}
+				for (int j = 0; j < n * 3; j++) {
+					sb.append(" ");
+				}
+				for (int j = 0; j < n; j++) {
+					sb.append("@");
+				}
+				sb.append("\n");
+			}
+
+			for (int i = 0; i < n; i++) {
+				for (int j = 0; j < n * 5; j++) {
+					sb.append("@");
+				}
+				sb.append("\n");
+			}
+
+			System.out.print(sb);
+		} catch (IOException e) {
+			throw new RuntimeException();
+		}
+	}
+}
