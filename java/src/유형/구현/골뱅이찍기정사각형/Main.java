94f6ff8136f887b43ed0ebd7331911b8d2456167
middlefitting <wjdtmdcjf2@gmail.com>
2025-01-20 09:09:23 +0900

    feat(solved.ac): 골뱅이찍기ㅋ

---
+++ java/src/유형/구현/골뱅이찍기정사각형/Main.java
@@ -0,0 +1,42 @@
+package 유형.구현.골뱅이찍기정사각형;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) {
+		try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+
+			int n = Integer.parseInt(br.readLine());
+
+			StringBuilder sb = new StringBuilder();
+
+			for (int j = 0; j < n + 2; j++) {
+				sb.append("@");
+			}
+			sb.append("\n");
+
+			for (int i = 0; i < n; i++) {
+				sb.append("@");
+
+				for (int j = 0; j < n; j++) {
+					sb.append(" ");
+				}
+
+				sb.append("@");
+
+				sb.append("\n");
+			}
+
+			for (int j = 0; j < n + 2; j++) {
+				sb.append("@");
+			}
+			sb.append("\n");
+
+			System.out.print(sb);
+		} catch (IOException e) {
+			throw new RuntimeException();
+		}
+	}
+}
